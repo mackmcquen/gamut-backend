163c1d82ec6f5b4e288248d7fae7db1dbd39f602
mackmcquen <mackmcquen@comcast.net>
2020-08-31 16:46:14 -0500

    CORS

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://gamutart.netlify.app/'
+    origins '*'
 
     resource '*',
       headers: :any,
